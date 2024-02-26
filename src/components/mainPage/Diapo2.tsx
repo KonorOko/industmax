18c21bd0dec767eb0b8a4314dbc28064884827d0
Konor <emilioxkonor@gmail.com>
2024-02-25 21:31:07 -0600

    Add more content to diapo 2

--- src/components/mainPage/Diapo2.tsx
+++ src/components/mainPage/Diapo2.tsx
@@ -3,7 +3,9 @@ export function Diapo2() {
     <div className="bg-blue-300 h-screen">
       <header>
         <h1 className="text-4xl md:text-8xl font-bold text-center text-black py-5">
-          <strong>¿Qué es Indust<span className="text-red-700">MAX</span>?</strong>
+          <strong>
+            ¿Qué es Indust<span className="text-red-700">MAX</span>?
+          </strong>
         </h1>
         <p className="text-lg md:text-4xl text-center mt-6">
           <strong>
@@ -12,11 +14,24 @@ export function Diapo2() {
           </strong>
         </p>
       </header>
-        <div className="w-full md:grid md:grid-cols-2">
-        <img src="https://cdn.illacloud.com/illa-website/blog/shadcn-ui-2024/shadcn-ui.png" alt="Imagen de una aplicación donde se llevan registros" className="aspect-video p-5 mx-auto"/>
-          <p className="md:text-2xl my-auto text-lg text-center font-semibold">
-            Podrás tener un control total de tus procesos de producción desde cualquier lugar, en cualquier momento y desde cualqueir dispositivo. 
+      <div className="w-full md:grid md:grid-cols-2">
+        <div className="p-10">
+          <img
+            src="https://cdn.illacloud.com/illa-website/blog/shadcn-ui-2024/shadcn-ui.png"
+            alt="Imagen de una aplicación donde se llevan registros"
+            className="aspect-video mx-auto rounded-lg border"
+          />
+        </div>
+        <div className="my-auto text-3xl">
+          <p className=" text-center font-semibold">
+            Podrás tener un control total de tus procesos de producción desde
+            cualquier lugar, en cualquier momento y desde cualquier dispositivo.
           </p>
+          <p className=" text-center font-semibold mt-10">
+            Además, podrás contar con nuestro equipo para agregar
+            funcionalidades personalizadas.
+          </p>
+        </div>
       </div>
     </div>
   );
