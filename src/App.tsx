a5ad20adc8e9a134b27ab1ffde179de123304c0a
Konor <emilioxkonor@gmail.com>
2024-03-11 14:19:59 -0600

    fix dots bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Diapo1 } from "@/components/mainPage/Diapo1";
 import { Diapo2 } from "@/components/mainPage/Diapo2";
 function App() {
   return (
-    <div className="overflow-x-hidden absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_2px,transparent_2px)] [background-size:16px_16px]">
+    <div className="overflow-x-hidden absolute inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] md:bg-[radial-gradient(#e5e7eb_2px,transparent_2px)] [background-size:16px_16px]">
       <Diapo1 />
       <Diapo2 />
     </div>
