70fdf6a2efb2a7fd78fba1879bf7b7f7677bdcec
Konor <emilioxkonor@gmail.com>
2024-03-12 09:01:12 -0600

    Fix bg mobile

--- src/components/personal-ui/Card.tsx
+++ src/components/personal-ui/Card.tsx
@@ -1,6 +1,6 @@
 export function Card({ title, desc, className, size = "md" }: { title: string; desc: string, className?: string, size?: string }) {
   return (
-    <div className={`shadow p-2 hover:scale-105 transition-all rounded-lg text-center bg-blue-300 ${size === "sm" && "md:h-52 md:w-48"} ${className}`} >
+    <div className={`shadow p-2 select-none hover:scale-105 transition-all rounded-lg text-center bg-blue-300 ${size === "sm" && "md:h-52 md:w-48"} ${className}`} >
       <h2 className="md:text-xl font-bold">{title}</h2>
       <p className="text-sm">{desc}</p>
     </div>
